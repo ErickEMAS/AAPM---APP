dc1817b87d3eb5f82dd96745fa8c74ec17f50007
lua ferraz <leticia.ferraz@7comm.com.br>
2022-04-20 21:00:16 -0300

    base structure

---
+++ agente_parceiro_magalu/lib/shared/themes/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primary = const Color(0xFF0F88FF);
+  static Color white = const Color(0xFFFFFFFF);
+  static Color black = const Color(0xFF000000);
+
+  //
+  static Color inputHint = const Color(0xFFA9A9A9);
+}
---
+++ agente_parceiro_magalu/lib/shared/themes/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {}
+
+class AppThemes {
+  static final theme = ThemeData();
+}
