d77ff9a13d335edaace8f7f624b0e70b046e39fa
lua ferraz <leticia.ferraz@7comm.com.br>
2022-05-01 20:59:32 -0300

    botom app bar

--- agente_parceiro_magalu/lib/shared/widgets/app_bottom_bar_widget.dart
+++ agente_parceiro_magalu/lib/shared/widgets/app_bottom_bar_widget.dart
@@ -1,91 +1,84 @@
+import 'package:flutter/material.dart';
 import 'package:agente_parceiro_magalu/app/home/presentation/stores/home_store.dart';
 import 'package:agente_parceiro_magalu/core/locators/service_locators.dart';
 import 'package:agente_parceiro_magalu/shared/themes/app_colors.dart';
-import 'package:flutter/material.dart';
-
 import '../../core/routes/app_routes.dart';
 
-class AppBottomBar extends StatefulWidget {
-  const AppBottomBar({
-    Key? key,
-  }) : super(key: key);
+class AppBottomBar extends StatelessWidget {
+  AppBottomBar({Key? key}) : super(key: key);
 
-  @override
-  State<AppBottomBar> createState() => _AppBottomBarState();
-}
-
-class _AppBottomBarState extends State<AppBottomBar> {
   final HomeStore _store = serviceLocator<HomeStore>();
-
-  bool isAdmin = false;
-  String? path;
-
-  @override
-  void initState() {
-    isAdmin = _store.userRole == "ROLE_ADMIN";
-    super.initState();
-  }
+  final bool isAdmin = false;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 56,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          bottomIcon(
-            tittle: "Home",
-            iconData: Icons.home,
-            active: _store.currentPage == AppRoutes.home,
-            onPressed: () {
-              _store.currentPage = AppRoutes.home;
-              _navigator(context: context, route: AppRoutes.home);
-            },
-          ),
-          isAdmin
-              ? bottomIcon(
-                  tittle: "Agentes",
-                  iconData: Icons.people,
-                  active: _store.currentPage == AppRoutes.agent,
-                  onPressed: () {
-                    _store.currentPage = AppRoutes.agent;
-                    _navigator(context: context, route: AppRoutes.agent);
-                  },
-                )
-              : Container(),
-          bottomIcon(
-            tittle: "Agenda",
-            iconData: Icons.calendar_today,
-            active: _store.currentPage == AppRoutes.calendar,
-            onPressed: () {
-              _store.currentPage = AppRoutes.calendar;
-              _navigator(context: context, route: AppRoutes.calendar);
-            },
-          ),
-          bottomIcon(
-            tittle: "Carteira",
-            iconData: Icons.credit_card,
-            active: _store.currentPage == AppRoutes.sellers,
-            onPressed: () {
-              _store.currentPage = AppRoutes.sellers;
-              _navigator(context: context, route: AppRoutes.sellers);
-            },
-          ),
-          bottomIcon(
-            tittle: "Minha Conta",
-            iconData: isAdmin ? Icons.admin_panel_settings : Icons.person,
-            active: _store.currentPage == AppRoutes.userAccount ||
-                path == AppRoutes.adminAccount,
-            onPressed: () {
-              _store.currentPage =
-                  isAdmin ? AppRoutes.adminAccount : AppRoutes.userAccount;
-              _navigator(
-                context: context,
-                route: isAdmin ? AppRoutes.adminAccount : AppRoutes.userAccount,
-              );
-            },
-          ),
-        ],
+    return BottomAppBar(
+      notchMargin: 2.0,
+      child: SizedBox(
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            bottomIcon(
+              tittle: "Home",
+              iconData: Icons.home,
+              active: _store.currentPage == AppRoutes.home,
+              onPressed: () {
+                _store.currentPage = AppRoutes.home;
+
+                _navigator(context: context, route: AppRoutes.home);
+              },
+            ),
+            isAdmin
+                ? bottomIcon(
+                    tittle: "Agentes",
+                    iconData: Icons.people,
+                    active: _store.currentPage == AppRoutes.agent,
+                    onPressed: () {
+                      _store.currentPage = AppRoutes.agent;
+
+                      _navigator(context: context, route: AppRoutes.agent);
+                    },
+                  )
+                : Container(),
+            bottomIcon(
+              tittle: "Agenda",
+              iconData: Icons.calendar_today,
+              active: _store.currentPage == AppRoutes.calendar,
+              onPressed: () {
+                _store.currentPage = AppRoutes.calendar;
+
+                _navigator(context: context, route: AppRoutes.calendar);
+              },
+            ),
+            bottomIcon(
+              tittle: "Carteira",
+              iconData: Icons.credit_card,
+              active: _store.currentPage == AppRoutes.sellers,
+              onPressed: () {
+                _store.currentPage = AppRoutes.sellers;
+
+                _navigator(context: context, route: AppRoutes.sellers);
+              },
+            ),
+            bottomIcon(
+              tittle: "Minha Conta",
+              iconData: isAdmin ? Icons.admin_panel_settings : Icons.person,
+              active: _store.currentPage == AppRoutes.userAccount ||
+                  _store.currentPage == AppRoutes.adminAccount,
+              onPressed: () {
+                _store.currentPage =
+                    isAdmin ? AppRoutes.adminAccount : AppRoutes.userAccount;
+
+                _navigator(
+                  context: context,
+                  route:
+                      isAdmin ? AppRoutes.adminAccount : AppRoutes.userAccount,
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
