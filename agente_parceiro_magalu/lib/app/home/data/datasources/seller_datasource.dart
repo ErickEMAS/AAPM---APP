6e28d057b11953c5c2782edd64a5c44be1de25d0
lua ferraz <leticia.ferraz@7comm.com.br>
2022-05-01 13:30:20 -0300

    add seller

--- agente_parceiro_magalu/lib/app/home/data/datasources/seller_datasource.dart
+++ agente_parceiro_magalu/lib/app/home/data/datasources/seller_datasource.dart
@@ -1,9 +1,5 @@
-import 'dart:convert';
-
-import 'package:agente_parceiro_magalu/app/auth/data/models/user_model.dart';
+import 'package:agente_parceiro_magalu/app/home/data/models/seller_model.dart';
 import 'package:agente_parceiro_magalu/core/constants/api_endpoints.dart';
-import 'package:agente_parceiro_magalu/core/constants/storage_keys.dart';
-import 'package:agente_parceiro_magalu/core/helpers/storage_helper.dart';
 import 'package:agente_parceiro_magalu/core/http/http_service.dart';
 import 'package:agente_parceiro_magalu/core/http/interceptor/auth_interceptor.dart';
 import 'package:agente_parceiro_magalu/core/locators/service_locators.dart';
@@ -11,6 +7,7 @@ import 'package:dio/dio.dart';
 
 abstract class ISellerDatasource {
   Future getSellerList();
+  Future postSeller({SellerModel? sellerModel});
 }
 
 class SellerDatasource implements ISellerDatasource {
@@ -29,6 +26,27 @@ class SellerDatasource implements ISellerDatasource {
     try {
       final response = await _httpWithAuth.get(
         Endpoints.getSellerListByAgentId,
+      );
+
+      print(response);
+    } on DioError catch (err) {
+      rethrow;
+    } catch (err) {
+      print(err);
+      rethrow;
+    }
+  }
+
+  @override
+  Future postSeller({SellerModel? sellerModel}) async {
+    try {
+      if (sellerModel == null) {
+        return;
+      }
+
+      final response = await _httpWithAuth.get(
+        Endpoints.postSeller,
+        data: sellerModel.toJson(),
       );
 
       print(response);
